1973b32b7a6ca14fddc664e4e398d53fa3d1bf0f
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2021-02-08 06:39:11 +0100

    Update index_save.py

--- index_save.py
+++ index_save.py
@@ -1,5 +1,9 @@
 from shutil import copyfile
+from datetime import datetime
 
 with open("index.php", "r") as to_check, open("index_compare.php") as to_compare:
     if to_check.read() != to_compare.read():
         copyfile("index_compare.php", "index.php")
+
+        with open("debug_log.txt", "a+") as logging:
+            logging.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}Malicious Software detected")
